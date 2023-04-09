359d49a9d1923a4dcb3f20bc5623917fc241d107
erichy23 <erichsyhuang@gmail.com>
2023-04-09 14:08:17 -0700

    Add tests for hist_plot function

---
+++ tests/testthat/test-hist_plot.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(testthat)
+library(vdiffr)
+
+# test input
+testthat::test_that("data_plot should return error when the data type of input
+                    argument is wrong", {
+  expect_error(hist_plot("iris", "Species"))
+  expect_error(hist_plot(iris, Species))
+})
+
+# test output
+testthat::test_that("data_plot fun returns the correct plots", {
+  pf <- hist_plot(iris, "Species")
+  expect_doppelganger("function returns correct hist", pf)
+  expect_equal(ggtitle(pf)$labels, ggtitle(helper_hist)$labels)
+})
+
